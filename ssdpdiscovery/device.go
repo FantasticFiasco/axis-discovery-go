93177ead66ffa3a210c7d24f4cebbc5d06305dc0
FantasticFiasco <mattias.kindborg@gmail.com>
2019-08-01 23:12:33 +0200

    wip

--- ssdpdiscovery/device.go
+++ ssdpdiscovery/device.go
@@ -2,14 +2,28 @@ package ssdpdiscovery
 
 import "net"
 
-// A Device represents a device on the network.
+// Device represents a network attached hardware unit from Axis Communications.
 type Device struct {
-	Addr             *net.UDPAddr // network address
-	Port             int          // network port
-	MACAddr          string       // In most situations this is identical to the serial number. The exceptions are the Axis products which bundle multiple physical devices into a single casing with a shared network interface. Because of the shared network interface they also share the same MAC address.
-	FriendlyName     string       // short description of the device
-	ModelName        string       // short model name
-	ModelDescription string       // long model description
-	ModelNumber      string       // short model number
-	PresentationURL  string       // URL to the web page of the device
+	// Addr specifies the TCP address.
+	Addr *net.TCPAddr
+
+	// MACAddr is in most situations identical to the serial number. The exceptions are the Axis products which bundle
+	// multiple physical devices into a single casing with a shared network interface. Because of the shared network
+	// interface they also share the same MAC address.
+	MACAddr string
+
+	// FriendlyName specifies the short description of the device.
+	FriendlyName string
+
+	// ModelName specifies the short model name.
+	ModelName string
+
+	// ModelDescription specifies the long model description.
+	ModelDescription string
+
+	// ModelNumber is the short model number.
+	ModelNumber string
+
+	// PresentationURL specifies URL to the web page of the device.
+	PresentationURL string
 }
