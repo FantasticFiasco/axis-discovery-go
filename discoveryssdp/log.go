158cfd14f6218b9287e0f867b0b5bb6838788ef6
Mattias Kindborg @FantasticFiasco <FantasticFiasco@users.noreply.github.com>
2019-08-03 11:57:22 +0200

    feat: debug logger (#1)

---
+++ discoveryssdp/log.go
@@ -0,0 +1,13 @@
+package discoveryssdp
+
+import "log"
+
+// Logger is an internal debug logger. Normally there is no need to expose the internal log messages, but if
+// there is a need for it the messages can be exposed by setting this variable.
+var Logger *log.Logger
+
+func logf(format string, v ...interface{}) {
+	if Logger != nil {
+		Logger.Printf(format, v...)
+	}
+}
